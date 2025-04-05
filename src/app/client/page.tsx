910d9a2a93218c397e23cd4375ca4604ace2c456
VladTimofte <vladtimofte999@gmail.com>
2025-04-05 21:47:08 +0300

    added onrender address

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -6,7 +6,7 @@ export default function ClientPage() {
   const videoRef = useRef<HTMLVideoElement>(null)
   const peerRef = useRef<RTCPeerConnection | null>(null)
   const socketRef = useRef<WebSocket | null>(null)
-  const signalingUrl = 'ws://betel-webrtc-stream-server.onrender.com'
+  const signalingUrl = 'wss://betel-webrtc-stream-server.onrender.com'
 
   useEffect(() => {
     const socket = new WebSocket(signalingUrl)
